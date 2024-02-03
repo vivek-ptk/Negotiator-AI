767242d78bcac38ce62fd7fab25e82d5a3f0508b
logically-nerd <adnansh2804@gmail.com>
2024-02-03 15:04:06 +0530

    navbar and data

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,8 +1,13 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [],
+  content: ["./src/**/*.{js,jsx,ts,tsx}",],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        "sec-color": "#FFCF96",
+        "prim-color": "#CDFADB"
+      }
+    },
   },
   plugins: [],
 }
