4794cf4b1c49c52bd02c524b672f89d5d3e07397
logically-nerd <adnansh2804@gmail.com>
2024-02-04 10:47:51 +0530

    searchbar add

---
+++ frontend/src/components/SearchBar.js
@@ -0,0 +1,28 @@
+import React, { useState } from 'react';
+// import SearchIcon from '@heroicons/react/outline/SearchIcon';
+import { IoSearch } from "react-icons/io5";
+
+const SearchBar = () => {
+    const [searchTerm, setSearchTerm] = useState('');
+
+    // Handle search functionality here (e.g., call an API, filter local data)
+    const handleSearch = (event) => {
+        setSearchTerm(event.target.value);
+        // ... your search logic here
+    };
+
+    return (
+        <div className="relative flex items-center w-2/3">
+            <input
+                type="text"
+                value={searchTerm}
+                onChange={handleSearch}
+                placeholder="Search..."
+                className="bg-gray-100 rounded-full pl-10 pr-4 py-2 focus:outline-none focus:ring-opacity-50 w-full"
+            />
+            <IoSearch className="absolute right-3 top-3 text-gray-400 h-5 w-5 cursor-pointer hover:text-blue-500" />
+        </div>
+    );
+};
+
+export default SearchBar;
